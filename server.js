8469f976ad12fa23779bd0e3521f2dafee389371
codice Web & Design <proyectos@codicewd.com>
2024-05-25 19:50:52 -0500

    prueba 2

--- server.js
+++ server.js
@@ -41,27 +41,27 @@ app.use(async (req, res, next) => {
  * Express Error Handler
  * Place after all other middleware
  *************************/
-// app.use(async (err, req, res, next) => {
-//   let nav = await utilities.getNav();
-//   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
-//   if (err.status == 404) {
-//     message = err.message;
-//   } else {
-//     message = "Oh no! There was a crash. Maybe try a different route?";
-//   }
-//   res.render("errors/error", {
-//     title: err.status || "Server Error",
-//     message,
-//     nav,
-//   });
-// });
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5432;
 const host = process.env.HOST;
 
 /* ***********************
