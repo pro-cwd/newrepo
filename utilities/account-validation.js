c5b6f8ad72a9906e14a75ad2bbc9ec19171c4a4d
codice Web & Design <proyectos@codicewd.com>
2024-08-03 15:57:59 -0500

    server-side validation

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,5 +1,6 @@
 const utilities = require(".");
 const { body, validationResult } = require("express-validator");
+const accountModel = require("../models/account-model");
 const validate = {};
 
 /*  **********************************
@@ -26,11 +27,17 @@ validate.registationRules = () => {
     // valid email is required and cannot already exist in the DB
     body("account_email")
       .trim()
-      .escape()
-      .notEmpty()
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(
+          account_email
+        );
+        if (emailExists) {
+          throw new Error("Email exists. Please log in or use different email");
+        }
+      }),
 
     // password is required and must be strong password
     body("account_password")
